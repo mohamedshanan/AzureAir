efe9d10ebf1b552d283a1f1a6d82456fdb4cdb7e
Mohamed Shannan <mohshanan87@gmail.com>
2019-07-30 00:28:59 +0200

    Refactor: use const val instead of hardcoded parameter names

--- data/src/main/java/com/shannan/azureair/data/remote/LufthansaApi.kt
+++ data/src/main/java/com/shannan/azureair/data/remote/LufthansaApi.kt
@@ -8,26 +8,14 @@ import retrofit2.http.*
 
 internal interface LufthansaApi {
 
-    companion object {
-        const val AIRPORT_DB_NAME: String = "Airports.db"
-        const val LH_GRANT_TYPE: String = "client_credentials"
-        const val AUTHORIZATION_HEADER: String = "Authorization"
-        const val BEARER: String = "Bearer "
-        const val DEFAULT_LANG: String = "en"
-
-        const val GET_ACCESS_TOKEN: String = "oauth/token"
-        const val GET_AIRPORTS: String = "references/airports"
-        const val GET_SCHEDULES: String = "operations/schedules/{origin}/{destination}/{fromDateTime}"
-    }
-
     /**
      * Get access token
      */
     @FormUrlEncoded
     @POST(GET_ACCESS_TOKEN)
-    fun authenticate(@Field("client_id") clientId: String,
-                     @Field("client_secret") clientSecret: String,
-                     @Field("grant_type") grantType: String):
+    fun authenticate(@Field(PARAM_CLIENT_ID) clientId: String,
+                     @Field(PARAM_CLIENT_SECRET) clientSecret: String,
+                     @Field(PARAM_GRANT_TYPE) grantType: String):
             Call<UserEntity>
 
     /**
@@ -35,20 +23,41 @@ internal interface LufthansaApi {
      */
     @GET(GET_AIRPORTS)
     fun getAirports(@Header(AUTHORIZATION_HEADER) token: String,
-                    @Query("offset") offset: Int,
-                    @Query("limit") limit: Int,
-                    @Query("lang") lang: String):
+                    @Query(PARAM_OFFSET) offset: Int,
+                    @Query(PARAM_LIMIT) limit: Int,
+                    @Query(PARAM_LANG) lang: String):
             Call<AirportsResponse>
 
     /**
      * Get a list of flight between two airports in the provided date
      */
     @GET(GET_SCHEDULES)
-    fun searchFlights(@Path("origin") origin: String,
-                      @Path("destination") destination: String,
-                      @Path("fromDateTime") fromDateTime: String,
+    fun searchFlights(@Path(PARAM_ORIGIN) origin: String,
+                      @Path(PARAM_DESTINATION) destination: String,
+                      @Path(PARAM_FROMDATETIME) fromDateTime: String,
                       @Header(AUTHORIZATION_HEADER) token: String,
-                      @Query("offset") offset: Int,
-                      @Query("limit") limit: Int):
+                      @Query(PARAM_OFFSET) offset: Int,
+                      @Query(PARAM_LIMIT) limit: Int):
             Call<FlightsResponse>
+
+    companion object {
+        const val LH_GRANT_TYPE: String = "client_credentials"
+        const val BEARER: String = "Bearer "
+        const val DEFAULT_LANG: String = "en"
+
+        const val GET_ACCESS_TOKEN = "oauth/token"
+        const val GET_AIRPORTS = "references/airports"
+        const val GET_SCHEDULES = "operations/schedules/{origin}/{destination}/{fromDateTime}"
+        const val AUTHORIZATION_HEADER: String = "Authorization"
+        const val PARAM_CLIENT_ID = "client_id"
+        const val PARAM_CLIENT_SECRET = "client_secret"
+        const val PARAM_GRANT_TYPE = "grant_type"
+        const val PARAM_OFFSET = "offset"
+        const val PARAM_LIMIT = "limit"
+        const val PARAM_LANG = "lang"
+
+        const val PARAM_ORIGIN = "origin"
+        const val PARAM_DESTINATION = "destination"
+        const val PARAM_FROMDATETIME = "fromDateTime"
+    }
 }
